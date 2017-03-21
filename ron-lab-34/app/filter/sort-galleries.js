98fb74230eecf903495c5424a97673c0704b1fe1
Rubiksron <rubiksron@gmail.com>
2017-03-20 20:04:19 -0700

    added fuzzy-search.js.

--- ron-lab-34/app/filter/sort-galleries.js
+++ ron-lab-34/app/filter/sort-galleries.js
@@ -1,48 +1,18 @@
 'use strict';
 
 module.exports = function() {
-  return function(galleries, str) {
-    if(!str) return galleries;
-
-    // let pattern = `.*${str.split('').join('.*')}.*`;
-    let pattern = '.*' + str.split('').join('.*') + '.*';
-    let regExp = new RegExp(pattern);
-    return galleries.filter(gallery => {
-      regExp.gallery.name
-    });
+  return function(galleries, limit=Infinity) {
+    return galleries.sort((a, b) => {
+      return b.name < a.name;
+    })
+    .slice(0,limit);
   };
 };
 
-//the below code is duncan's from slugram, comparing notes here...
-// module.exports = function(input){
-//   debug(`creating fuzzy regex from ${input}`)
-//   if (!input || typeof input !== 'string')
-//     return new RegExp('.*')
-//   let result = input.split('').join('.*')
-//   return new RegExp(`.*${result}.*`)
-// }
-
-
-
-
-
-
-
-
-
-// module.exports = function() {
-//   return function(galleries, limit=Infinity) {
-//     return galleries.sort((a, b) => {
-//       return b.name < a.name;
-//     })
-//     .slice(0,limit);
-//   };
-// };
-//
-// module.exports = function() {
-//   function genReg(str) {
-    // let pattern = '.*' + str.split('').join('.*') + '.*';
-//     return new RegExp(pattern);
-//   }
-//   console.log(genReg('this is my string'));
-// };
+module.exports = function() {
+  function genReg(str) {
+    let pattern = '.*' + str.split('').join('.*') + '.*';
+    return new RegExp(pattern);
+  }
+  console.log(genReg('this is my string'));
+};
